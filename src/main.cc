d8cd4fc0b0b9653aa064e1b374f8f0b036b21dca
VC Science Labs <travis_chamness1@my.vcccd.edu>
2018-09-18 08:40:55 -0700

    Trying again

---
+++ src/main.cc
@@ -0,0 +1,18 @@
+/**
+* CS V13 Object-Oriented Programming
+* CRN: 70226
+* Assignment: EX01-HelloWorld
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+* 
+* @author travis_chamness1@my.vcccd.edu
+*/
+
+#include <iostream>
+//Very cool, hope this does it
+
+int main(int argc, int *argv){ 
+		std::cout << "Hello, World!" << std::endl;
+	return EXIT_SUCCESS;
+}
